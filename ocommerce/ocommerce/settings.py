bf340aca271562dbe3fcb679dd9bac26a7f1742f
Onesinus SPT <onesinus.parulian@bidbox.id>
2021-03-14 19:10:14 +0700

    Set static configuration to default

--- ocommerce/ocommerce/settings.py
+++ ocommerce/ocommerce/settings.py
@@ -124,11 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-if DEBUG is False:
-    STATIC_URL = '/static/'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-else:
-    STATIC_URL = '/static/'
-    STATICFILES_DIRS = [
-        BASE_DIR / "static",
-    ]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
