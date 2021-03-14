5c9f266f7bc6270b478134ef74c981d635d53121
Onesinus SPT <onesinus.parulian@bidbox.id>
2021-03-14 14:14:14 +0700

    Change db to sqlite for deployment purpose

--- ocommerce/ocommerce/settings.py
+++ ocommerce/ocommerce/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'ocommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': '',
-        'NAME': 'ocommerce',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
