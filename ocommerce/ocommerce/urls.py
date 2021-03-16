593b92b177778db571c2e8303b8757fd876ad550
Onesinus SPT <onesinus.parulian@bidbox.id>
2021-03-16 23:01:33 +0700

    Add Whitenoise to solve staticfiles

--- ocommerce/ocommerce/urls.py
+++ ocommerce/ocommerce/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import HomeView
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
     path('contactme/', include('contactme.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
