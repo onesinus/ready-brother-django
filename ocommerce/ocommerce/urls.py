db0a3f3e0f74adc212b3f25b46450085c2d7b605
Onesinus SPT <onesinus.parulian@bidbox.id>
2021-03-16 22:36:53 +0700

    Add static in urls.py

--- ocommerce/ocommerce/urls.py
+++ ocommerce/ocommerce/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import HomeView
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
     path('contactme/', include('contactme.urls'))
 ]
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
